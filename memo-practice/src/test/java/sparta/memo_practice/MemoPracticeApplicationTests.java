890734d14c9aaff906c7ba0d3738210006fe3cc1
dbp-jack <dbp100402@gmail.com>
2025-01-27 03:17:02 +0900

    스파르타 스프링 입 16.메모장

---
+++ memo-practice/src/test/java/sparta/memo_practice/MemoPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package sparta.memo_practice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MemoPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
