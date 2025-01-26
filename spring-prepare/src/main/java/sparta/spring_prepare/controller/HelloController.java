dcf60cc2fdd25a6a40cf6209ca99f35f620f3ec3
dbp-jack <dbp100402@gmail.com>
2025-01-26 15:28:36 +0900

    10. Controller Controller - 요청, 응답(비즈니스로직)을 처리한다. 서비스계층(view)으로 전달 및 반환(처리할 메서드와 매핑) 서블릿에 API를 담아서 처리한다. (직접 비즈니스 로직을 구현하지는 않는다.)
    @Mapping("/url") - 경로 중복 가능
    단, 중복을 불필요하기에
    @RequestMapping("/url")로 중복제거
    @Controller - view를 반환
    @RestController - 데이터 반환

--- spring-prepare/src/main/java/sparta/spring_prepare/controller/HelloController.java
+++ spring-prepare/src/main/java/sparta/spring_prepare/controller/HelloController.java
@@ -1,12 +1,34 @@
 package sparta.spring_prepare.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
 
-@RestController
+@Controller
+@RequestMapping("/api")
 public class HelloController {
-    @GetMapping("api/hello")
+    @GetMapping("/hello")
+    @ResponseBody
     public String hello(){
-        return "스프링 입문";
+        return "스프링 입문, 컨트롤러까지 왔다."; // view정보 전달
+    }
+    @GetMapping("/get")
+    @ResponseBody
+    public String get(){
+        return "GET Method 요청";
+    }
+    @PostMapping("/post")
+    @ResponseBody
+    public String post(){
+        return "POST Method 요청";
+    }
+    @PutMapping("/put")
+    @ResponseBody
+    public String put(){
+        return "PUT Method 요청";
+    }
+    @DeleteMapping("/delete")
+    @ResponseBody
+    public String delete(){
+        return "DELETE Method 요청";
     }
 }
