0dbba2a8f51dc259821e1bf9fd46d6aa18f0f207
dbp-jack <dbp100402@gmail.com>
2025-01-26 13:16:12 +0900

    5. 테스트 - 테스트가 필요한 이유
    테스트는 2가지
    블랙박스 테스팅 - SW내부구조를 모르는 사용자 입장에서 테스트
    개발자 테스트 - 직접 테스트 코드 작성. 유지보수비용, 개발기간이 더 오래걸린다.
    
    스프링에서는 테스트할 수 있게 제공핻준다. - junit
    
    테스트파일 만드는 방법 - 커+N, 커+T

---
+++ spring-prepare/src/test/java/sparta/spring_prepare/calculator/CalculatorTest.java
@@ -0,0 +1,31 @@
+package sparta.spring_prepare.calculator;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+// 커 + T - 테스트 생성
+class CalculatorTest {
+
+    @Test
+    @DisplayName("더하기 테스트")
+    void test1() {
+        Calculator calculator = new Calculator();
+        Double result = calculator.operate(8, "+", 2);
+        System.out.println("result = "+result);
+
+        Assertions.assertEquals(10, result); //5주차에 나온다 참고, expected 예상하는 값
+    }
+
+    @Test
+    @DisplayName("나누기 테스트")
+    void test2() {
+        Calculator calculator = new Calculator();
+        Double result = calculator.operate(8, "/", 2);
+        System.out.println("result = "+result);
+
+        Assertions.assertEquals(4, result);
+    }
+}
